3e1ed3986bc64b0f9aeeb1ec5a391f184b90600b
Mina Medhat <minamedhat123456@gmail.com>
2024-05-28 13:38:33 +0300

    Owner&Customer

--- Core/Entities/ApplicationUser.cs
+++ Core/Entities/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Core.Enums;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,14 @@ namespace Core.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SSN { get; set; }
+        public string Address { get; set; }
+        public string? ProfileImage { get; set; }
+        public bool IsBlocked { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+        public string YourFavirotePerson { get; set; }
+
     }
 }
--- Infrastructure/ApplicationDBContext.cs
+++ Infrastructure/ApplicationDBContext.cs
@@ -1,17 +1,43 @@
 ﻿using Core.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure
 {
     public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
-
         }
 
         public DbSet<City> Cities { get; set; }
         public DbSet<Governorate> Governorates { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasDiscriminator<string>("Discriminator")
+                .HasValue<Customer>("Customer")
+                .HasValue<Owner>("Owner");
+
+            modelBuilder.Entity<Owner>(entity =>
+            {
+                entity.Property(o => o.IDFrontImage).IsRequired();
+                entity.Property(o => o.IDBackImage).IsRequired();
+                entity.Property(o => o.UserType).IsRequired();
+                entity.Property(o => o.IsApproved).HasDefaultValue(false);
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                // configuration for Customer can go here
+            });
+
+        }
 
     }
 }
