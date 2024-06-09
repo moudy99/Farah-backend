b1a0e1cdc1e2027004b49226516ba4eaddcf9b41
moudy99 <bobmoudy1@gmail.com>
2024-06-09 23:23:21 +0300

    add migration

---
+++ Infrastructure/Migrations/20240609151044_init_4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServicesTypr",
+                table: "Services",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServicesTypr",
+                table: "Services");
+        }
+    }
+}
