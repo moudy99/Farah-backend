2a759fa09d2448aebc4b5d61c4174984e48253e2
Mina Medhat <minamedhat123456@gmail.com>
2024-06-15 06:12:12 +0300

    List

--- Application/Interfaces/IBeautyService.cs
+++ Application/Interfaces/IBeautyService.cs
@@ -6,7 +6,7 @@ namespace Application.Interfaces
     {
 
         CustomResponseDTO<List<BeautyCenterDTO>> GetAllBeautyCenters(int page, int pageSize);
-        CustomResponseDTO<ServiceForBeautyCenterDTO> AddBeautyService(ServiceForBeautyCenterDTO beautyDTO);
+        CustomResponseDTO<List<ServiceForBeautyCenterDTO>> AddBeautyService(List<ServiceForBeautyCenterDTO>beautyDTO);
         CustomResponseDTO<List<BeautyCenterDTO>> GetBeautyCenterByName(string name);
         CustomResponseDTO<BeautyCenterDTO> GetBeautyCenterById(int id);
         Task<CustomResponseDTO<BeautyCenterDTO>> AddBeautyCenter(AddBeautyCenterDTO beautyCenterDTO);
