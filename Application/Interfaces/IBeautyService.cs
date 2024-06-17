1cbed2e4d4eb1a9493dc024c34d3c0426b02f75f
moudy99 <bobmoudy1@gmail.com>
2024-06-17 19:01:24 +0300

    add confirm email by OTP

--- Application/Interfaces/IBeautyService.cs
+++ Application/Interfaces/IBeautyService.cs
@@ -5,12 +5,12 @@ namespace Application.Interfaces
     public interface IBeautyService
     {
 
-        CustomResponseDTO<List<BeautyCenterDTO>> GetAllBeautyCenters(int page, int pageSize, int govId , int cityId);
+        CustomResponseDTO<List<BeautyCenterDTO>> GetAllBeautyCenters(int page, int pageSize, int govId, int cityId);
 
 
-        CustomResponseDTO<List<BeautyCenterDTO>> GetAllBeautyCenters(int page, int pageSize);
+        //CustomResponseDTO<List<BeautyCenterDTO>> GetAllBeautyCenters(int page, int pageSize);
 
-        CustomResponseDTO<List<ServiceForBeautyCenterDTO>> AddBeautyService(List<ServiceForBeautyCenterDTO>beautyDTO);
+        CustomResponseDTO<List<ServiceForBeautyCenterDTO>> AddBeautyService(List<ServiceForBeautyCenterDTO> beautyDTO);
 
         CustomResponseDTO<List<BeautyCenterDTO>> GetBeautyCenterByName(string name);
         CustomResponseDTO<BeautyCenterDTO> GetBeautyCenterById(int id);
