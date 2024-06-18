c9a6f5525acb2273f87a69e8df1d16ce3ff72d1f
Mina Medhat <minamedhat123456@gmail.com>
2024-06-18 20:34:09 +0300

    Fix Photographer Delete

---
+++ Infrastructure/Migrations/20240618173124_delete-photographer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletephotographer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+    name: "FK_ImagePhotography_Photograph_PhotographerId",
+    table: "ImagePhotography");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImagePhotography_Photograph_PhotographerId",
+                table: "ImagePhotography",
+                column: "PhotographerId",
+                principalTable: "Photograph",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+    name: "FK_ImagePhotography_Photograph_PhotographerId",
+    table: "ImagePhotography");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImagePhotography_Photograph_PhotographerId",
+                table: "ImagePhotography",
+                column: "PhotographerId",
+                principalTable: "Photograph",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
