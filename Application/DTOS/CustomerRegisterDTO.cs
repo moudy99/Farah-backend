27729363926ac1410cf0ac911c978c3dc67d67bd
moudy99 <bobmoudy1@gmail.com>
2024-06-21 20:10:40 +0300

    Add insertUser Queries and update register DTO

--- Application/DTOS/CustomerRegisterDTO.cs
+++ Application/DTOS/CustomerRegisterDTO.cs
@@ -6,6 +6,8 @@
         public string LastName { get; set; }
         //public string UserName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
         public string SSN { get; set; }
         public int GovID { get; set; }
         public int CityID { get; set; }
