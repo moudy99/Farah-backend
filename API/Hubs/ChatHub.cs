d1c3d291a580a40a34b67e04f7bc7dbc8fccaa81
moudy99 <bobmoudy1@gmail.com>
2024-06-26 05:11:27 +0300

    update chatHub

--- API/Hubs/ChatHub.cs
+++ API/Hubs/ChatHub.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace Presentation.Hubs
 {
+    [Authorize]
     public class ChatHub : Hub
     {
-      
+
+
+        public override Task OnConnectedAsync()
+        {
+            var userId = Context.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId != null)
+            {
+            }
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userId = Context.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId != null)
+            {
+            }
+            return base.OnDisconnectedAsync(exception);
+        }
     }
 }
