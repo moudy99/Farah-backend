2d394bad8893cc0a87c90775601c7ee0d38193cb
Mina Medhat <minamedhat123456@gmail.com>
2024-07-05 22:17:13 +0300

    Fix Delete Beauty

---
+++ Infrastructure/Migrations/20240705190411_Delete_Beauty.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Delete_Beauty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImagesBeautyCenter_BeautyCenter_BeautyCenterId",
+                table: "ImagesBeautyCenter",
+                column: "BeautyCenterId",
+                principalTable: "BeautyCenter",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceForBeautyCenter_BeautyCenter_BeautyCenterId",
+                table: "ServiceForBeautyCenter",
+                column: "BeautyCenterId",
+                principalTable: "BeautyCenter",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavoriteService_Service_ServiceId",
+                table: "FavoriteService",
+                column: "ServiceId",
+                principalTable: "Service",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+    name: "FK_ImagesBeautyCenter_BeautyCenter_BeautyCenterId",
+    table: "ImagesBeautyCenter");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceForBeautyCenter_BeautyCenter_BeautyCenterId",
+                table: "ServiceForBeautyCenter");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavoriteService_Service_ServiceId",
+                table: "FavoriteService");
+        }
+    }
+}
